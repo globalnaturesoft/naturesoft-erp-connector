8111dc0f15f0c7baca6bc0785828cfbce9a3613d
Luan Pham <luanpm88@gmail.com>
2016-10-03 20:20:01 -0700

    fix unicode url bugs

--- app/controllers/naturesoft/erp_connector/admin/products_controller.rb
+++ app/controllers/naturesoft/erp_connector/admin/products_controller.rb
@@ -3,7 +3,11 @@ module Naturesoft
     module Admin
       class ProductsController < Naturesoft::Admin::AdminController
         def index
-          url = Option.get("erp_connector", "erp_product_url") + "?keyword=" + params[:keyword].to_s + "&page=" + params[:page].to_s
+          url = Option.get("erp_connector", "erp_product_url") + 
+                "?keyword=" + 
+                URI.encode(params[:keyword].to_s) + 
+                "&page=" +
+                URI.encode(params[:page].to_s)
           begin
             resp = Net::HTTP.get_response(URI.parse(url))
             data = resp.body
